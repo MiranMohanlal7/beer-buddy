da9fc9e811320c804bab8f192e8df2b2f3736eb8
Sam Ketelaars <576431@student.fontys.nl>
2025-11-14 14:22:16 +0100

    merge test

--- backend/beerbuddy.Api/obj/Debug/net10.0/beerbuddy.Api.AssemblyInfo.cs
+++ backend/beerbuddy.Api/obj/Debug/net10.0/beerbuddy.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beerbuddy.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bc933f3a70ec16e811fe835f2f28a219d1f8330")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb4e536ccfe661a523238ea50e97b6f14d660018")]
 [assembly: System.Reflection.AssemblyProductAttribute("beerbuddy.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beerbuddy.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
