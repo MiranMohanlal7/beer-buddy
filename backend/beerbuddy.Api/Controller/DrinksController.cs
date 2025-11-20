7eb4594ffd0f91304fc40ac7393fcac66d74b8db
Sam Ketelaars <576431@student.fontys.nl>
2025-11-20 13:09:43 +0100

    test1

--- backend/beerbuddy.Api/Controller/DrinksController.cs
+++ backend/beerbuddy.Api/Controller/DrinksController.cs
@@ -12,8 +12,8 @@ public class DrinksController : ControllerBase
     {
         new Drink { Id = 1, Name = "Heineken", Quantity = 12, Threshold = 6, ImageUrl = "https://dummyimage.com/100x100/000/fff&text=Heineken" },
         new Drink { Id = 2, Name = "Corona", Quantity = 8, Threshold = 4, ImageUrl = "https://dummyimage.com/100x100/000/fff&text=Corona" },
-        new Drink { Id = 3, Name = "Coca Cola", Quantity = 5, Threshold = 3, ImageUrl = "https://dummyimage.com/100x100/000/fff&text=RedBull" },
-        new Drink { Id = 4, Name = "Fanta", Quantity = 26, Threshold = 10, ImageUrl = "https://dummyimage.com/100x100/000/fff&text=CocaCola" }
+        new Drink { Id = 3, Name = "Coca Cola", Quantity = 5, Threshold = 3, ImageUrl = "https://dummyimage.com/100x100/000/fff&text=CocaCola" },
+        new Drink { Id = 4, Name = "Fanta", Quantity = 26, Threshold = 10, ImageUrl = "https://dummyimage.com/100x100/000/fff&text=Fanta" }
     };
 
     [HttpGet]
