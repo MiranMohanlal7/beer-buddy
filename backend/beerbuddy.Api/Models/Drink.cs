28d93b38b45a6354cc55666aa9e3e849c28a0a4f
Sam Ketelaars <576431@student.fontys.nl>
2025-11-27 10:44:34 +0100

    connection string virtual machine

--- backend/beerbuddy.Api/Models/Drink.cs
+++ backend/beerbuddy.Api/Models/Drink.cs
@@ -3,8 +3,8 @@ namespace beerbuddy.Api.Models;
 public class Drink
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public required string Name { get; set; }
     public int Quantity { get; set; }
     public int Threshold { get; set; }
-    public string ImageUrl { get; set; }
+    public required string ImageUrl { get; set; }
 }
